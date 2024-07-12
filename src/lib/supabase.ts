2212b5d6a8dd5dd3da03b677f31f4531c045add8
benny.thomsen20 <benny.thomsen20@gmail.com>
2024-07-12 20:44:25 +0000

    url as before

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://tenqlpcmcforcjxscaxw.supabase.co" as string
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 const supabase = createClient(supabaseUrl, supabaseKey)
 
